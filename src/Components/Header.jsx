dc76f7f7000aa8ddd9d637f543daaf2f50ae370b
DonDeWaay <dondewaay@gmail.com>
2024-03-05 13:19:00 -0600

    Start

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="inner-header">
+        <h1>Don DeWaay III</h1>
+        <h1>Contact</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
