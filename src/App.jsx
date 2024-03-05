a0f7bd653aa99916a5b2ef02fc219fa49ead616a
DonDeWaay <dondewaay@gmail.com>
2024-03-05 15:13:07 -0600

    More design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./Components/Header";
 import Background from "/img/1C.png";
 import Welcome from "./Components/Welcome";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 import "./Styles/App.scss";
 
@@ -12,16 +12,23 @@ function App() {
     <>
       <Header />
       <div className="content">
-        <img src={Background} />
-        {section == 1 ? (
-          <Welcome />
-        ) : section == 2 ? (
-          <Welcome />
-        ) : section == 3 ? (
-          <Welcome />
-        ) : (
-          <Welcome />
-        )}
+        <AnimatePresence>
+          <motion.img
+            src={Background}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.1, duration: 1 }}
+          />
+          {section == 1 ? (
+            <Welcome />
+          ) : section == 2 ? (
+            <Welcome />
+          ) : section == 3 ? (
+            <Welcome />
+          ) : (
+            <Welcome />
+          )}
+        </AnimatePresence>
         <ul>
           <li onClick={() => setSection(0)}>Welcome</li>
           <li onClick={() => setSection(1)}>Work</li>
