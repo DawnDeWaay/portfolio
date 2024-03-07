e460267a9ca33bf3fceb79d9c8a8b76bfa5a0753
DonDeWaay <dondewaay@gmail.com>
2024-03-07 16:16:10 -0600

    Woo

--- src/Components/Passions.jsx
+++ src/Components/Passions.jsx
@@ -1,6 +1,8 @@
 const Passions = () => {
   return (
-    <div className="page">Don Loves Fashion, Fighting Games, and Cooking</div>
+    <div className="page">
+      Don Loves Fashion, Design, Fighting Games, and Cooking
+    </div>
   );
 };
 
