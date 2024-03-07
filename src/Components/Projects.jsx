511098e2d21ecc8c2137a7bc21a96c176db81b1a
DonDeWaay <dondewaay@gmail.com>
2024-03-07 16:26:19 -0600

    Overflow

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,11 +1,7 @@
 import { motion } from "framer-motion";
 
 const Projects = () => {
-  return (
-    <motion.h2 className="page">
-      MultiRoll, a Dungeons and Dragons Companion
-    </motion.h2>
-  );
+  return <h2 className="page">MultiRoll, a Dungeons and Dragons Companion</h2>;
 };
 
 export default Projects;
