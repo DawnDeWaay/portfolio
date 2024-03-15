72a4ddfad3b78f95289b80a24d3497d88a05d405
DonDeWaay <dondewaay@gmail.com>
2024-03-15 17:32:21 -0500

    Fixed stupid box hover issue

--- src/Components/SketchItem.jsx
+++ src/Components/SketchItem.jsx
@@ -5,27 +5,31 @@ const SketchItem = ({ name, id, date, img }) => {
 
   return (
     <motion.div
-      className="item"
       initial={{ opacity: 0, y: 20 }}
       animate={{
-        opacity: 0.7,
+        opacity: 1,
         y: 0,
         transition: {
           duration: 0.3,
           delay: delayTime,
         },
       }}
-      whileHover={{
-        opacity: 1,
-        transition: { duration: 0.7 },
-      }}
-      whileTap={{ scale: 0.99 }}
       exit={{ opacity: 0, y: 20 }}
     >
-      <div className="name">{name}</div>
-      <div className="id">No.{id}</div>
-      <img src={img} alt={id} className="drawing" />
-      <div className="date">{date}</div>
+      <motion.div
+        className="item"
+        initial={{ opacity: 0.7 }}
+        whileHover={{
+          opacity: 1,
+          transition: { duration: 0.3 },
+        }}
+        whileTap={{ scale: 0.99 }}
+      >
+        <div className="name">{name}</div>
+        <div className="id">No.{id}</div>
+        <img src={img} alt={id} className="drawing" />
+        <div className="date">{date}</div>
+      </motion.div>
     </motion.div>
   );
 };
