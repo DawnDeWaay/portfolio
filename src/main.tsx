17b86cf841e8bd50a86d5910956164d0dfee64ed
DonDeWaay <dondewaay@gmail.com>
2024-11-18 18:47:21 -0600

    yippee

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./output.css";
+import "./style.scss";
 import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
