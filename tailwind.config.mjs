e5e67dbef4ea2a2c701d93583ca46bcfa2c5994c
DonDeWaay <dondewaay@gmail.com>
2025-01-17 03:05:16 -0600

    mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-/*eslint-env node*/
-/*eslint no-undef: "error"*/
-
-module.exports = {
+export default {
   content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
   theme: {
     extend: {},
