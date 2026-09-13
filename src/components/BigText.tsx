3d637066346044a849f5f363d36533b530d03a6b
DonDeWaay <dondewaay@gmail.com>
2026-09-12 19:36:16 -0500

    hm

--- src/components/BigText.tsx
+++ src/components/BigText.tsx
@@ -5,7 +5,7 @@ const BigText = ({ text }: { text: string }) => {
     <>
       <div id={text} />
       <motion.h1
-        className="sticky pointer-events-none text-[22vw] pb-[-5vw] flex w-[100%] leading-[1.1] top-16 text-nowrap overflow-x-hidden overflow-y-visible z-[-1]"
+        className="pointer-events-none text-[22vw] pb-[-5vw] flex w-[100%] leading-[1.1] top-16 text-nowrap overflow-x-hidden overflow-y-visible z-[-1]"
         initial={{ y: "-1vw" }}
       >
         <span className="redaction35 text-[#796C98]">&nbsp;~ </span>
