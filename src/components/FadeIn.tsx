7c8716bdfaa89d5575f4ac2124d80db6028eb662
DonDeWaay <dondewaay@gmail.com>
2026-09-22 15:32:32 -0500

    ourple

--- src/components/FadeIn.tsx
+++ src/components/FadeIn.tsx
@@ -4,7 +4,7 @@ const FadeIn = () => {
 			className="pointer-events-none fixed top-0 left-0 h-[30vh] w-full z-30"
 			style={{
 				background:
-					"linear-gradient(to bottom, #f2f1ec 0%, #f2f1ec 40%, transparent 100%)",
+					"linear-gradient(to bottom, #f1eef4 0%, #f1eef4 40%, transparent 100%)",
 			}}
 		/>
 	);
